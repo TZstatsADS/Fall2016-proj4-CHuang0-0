e564f31733fbb17e90791c0bd06aa72de0f50b86
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-15 12:46:41 -0500

    Update R code_Ch3129.R

--- lib/R code_Ch3129.R
+++ lib/R code_Ch3129.R
@@ -2,6 +2,8 @@
 # Name: Chenxi Huang
 # ch3129
 
+# set the workng directory 
+setwd("C:/Users/ch3129/Desktop/Project 4")
 ###################################################################################################
 # Intro To Dataset
 #Common_id.txt: ids for the songs that have both lyrics and sound analysis information. 2350 in total;
@@ -15,4 +17,23 @@
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
 ###################################################################################################
+# Load Packages
 library(rhdf5)
+
+
+###################################################################################################
+# The structure of a song
+#h5ls("/Users/Bianbian/Documents/Courses/2016Spring/4249_Applied Data Science/Proj5/MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5")
+read1=h5ls('C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5') #try out an example
+dim(read1);summary(read1)
+# the data are stored in three groups, analysis, metadata and musicbrainz
+# Get all data using h5read
+sound=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/analysis")
+meta=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/metadata")
+musicbrainz=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/musicbrainz")
+sound
+meta
+musicbrainz
+summary(sound)
+summary(meta)
+summary(musicbrainz)
