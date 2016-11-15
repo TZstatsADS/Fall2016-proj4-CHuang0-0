9f43058dcf0820e5f2ce6b7db907a779326186ad
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-15 14:49:04 -0500

    Update tutorial_celia.R

--- lib/tutorial_celia.R
+++ lib/tutorial_celia.R
@@ -1,5 +1,8 @@
 install.packages("devtools")
 devtools::install_github("cpsievert/LDAvisData")
+slam_url <- "http://cran.r-project.org"
+install.packages("slam", repos = slam_url, type = "source")
+install.packages("tm", repos="http://cran.rstudio.com/", dependencies=TRUE)
 
 library(NLP)
 library(tm)
@@ -32,3 +35,16 @@ length(doc.list)       # Length: number of documents
 length(doc.list[[1]])  # A splited string of words
 doc.list[1]
 doc.list[2]
+
+
+# compute the table of terms:
+term.table <- table(unlist(doc.list))
+term.table <- sort(term.table, decreasing = TRUE) 
+head(term.table)      # Just to give an idea of how it looks like
+dim(term.table) # 39399
+head(term.table,n=20) #head() returns the top 20 words
+
+# remove terms that are stop words or occur fewer than 5 times:
+del <- names(term.table) %in% stop_words | term.table < 5
+term.table <- term.table[!del]
+vocab <- names(term.table)           # Vocab: a vector of all words
