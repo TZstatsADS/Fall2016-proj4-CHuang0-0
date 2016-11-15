4c33477a46dd1b6e1f57745fe45fc99693680780
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-15 14:14:56 -0500

    Update R code_Ch3129.R

--- lib/R code_Ch3129.R
+++ lib/R code_Ch3129.R
@@ -16,24 +16,37 @@ setwd("C:/Users/ch3129/Desktop/Project 4")
 #Download rhdf5 library
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
+# install missing packages
+list.of.packages <- c("NLP", "tm","lda","LDAvis","slam")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 ###################################################################################################
 # Load Packages
 library(rhdf5)
 
+# Topic Modeling
+library(NLP)
+library(tm);library(slam)# slam not available for R version 3.2.3
+library(lda)
+library(LDAvis)
 
 ###################################################################################################
 # The structure of a song
 #h5ls("/Users/Bianbian/Documents/Courses/2016Spring/4249_Applied Data Science/Proj5/MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5")
 read1=h5ls('C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5') #try out an example
-dim(read1);summary(read1)
+dim(read1);summary(read1);read1 #dim=27 by 5
 # the data are stored in three groups, analysis, metadata and musicbrainz
 # Get all data using h5read
 sound=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/analysis")
 meta=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/metadata")
 musicbrainz=h5read("C:/Users/ch3129/Desktop/Project 4/Project4_data/data/A/A/A/TRAAABD128F429CF47.h5", "/musicbrainz")
 sound
-meta
-musicbrainz
 summary(sound)
+meta
 summary(meta)
+musicbrainz
 summary(musicbrainz)
+# for example
+sound$segments_confidence;dim(sound$segments_confidence);class(sound$segments_confidence)  
+###################################################################################################
+
