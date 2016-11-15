ae5f433fea821e16fc9e8f03531f8959719bf11d
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-15 16:02:35 -0500

    Update tutorial_celia.R

--- lib/tutorial_celia.R
+++ lib/tutorial_celia.R
@@ -48,3 +48,57 @@ head(term.table,n=20) #head() returns the top 20 words
 del <- names(term.table) %in% stop_words | term.table < 5
 term.table <- term.table[!del]
 vocab <- names(term.table)           # Vocab: a vector of all words
+vocab
+length
+length(vocab) # length=14263
+term.table 
+dim(term.table) #dim=14263
+
+# now put the documents into the format required by the lda package:
+get.terms <- function(x) {
+  index <- match(x, vocab)
+  index <- index[!is.na(index)]
+  rbind(as.integer(index-1), as.integer(rep(1, length(index))))
+}
+documents <- lapply(doc.list, get.terms)
+dim(documents)
+documents[1] 
+names(documents[1])
+class(documents[1])
+dim(documents[1])
+doc.list_1=doc.list[1]
+doc.list_1
+doc.list_1=as.array(doc.list_1)
+class(doc.list_1);length(doc.list_1)
+vocab[9] 
+
+# DOCUMENTS is basically a large list of all docoments we have, each entry is a 2*length(document_i) matrix, recording which word in the VOCAB appeared in this document.
+
+# Compute some statistics related to the data set:
+D <- length(documents)  # number of documents (2,000)
+D 
+W <- length(vocab)  # number of terms in the vocab (14,568)
+W
+doc.length <- sapply(documents, function(x) sum(x[2, ]))  # number of tokens per document [312, 288, 170, 436, 291, ...]
+doc.length
+N <- sum(doc.length)  # total number of tokens in the data (546,827)
+N #545313
+term.frequency <- as.integer(term.table)  # frequencies of terms in the corpus [8939, 5544, 2411, 2410, 2143, 
+term.frequency
+
+# MCMC and model tuning parameters:
+K <- 20
+G <- 5000
+alpha <- 0.02
+eta <- 0.02
+
+# Fit the model:
+library(lda)
+set.seed(357)
+t1 <- Sys.time()
+fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab, 
+                                   num.iterations = G, alpha = alpha, 
+                                   eta = eta, initial = NULL, burnin = 0,
+                                   compute.log.likelihood = TRUE)
+t2 <- Sys.time()
+t2 - t1  # about 24 minutes on laptop
